16a0c81b859c79919b6c1696d14f59c1131e9000
Dylan Jones <jonesdyn@gmail.com>
2014-12-29 20:46:57 -0600

    Updating database script for new schema (issue #19)

--- script/createDatabase.sql
+++ script/createDatabase.sql
@@ -18,3 +18,37 @@ CREATE TABLE user_roles (
    UNIQUE KEY uni_username_role (role, username),
    KEY fk_username_idx (username),
    CONSTRAINT fk_username FOREIGN KEY (username) REFERENCES users (username));
+   
+CREATE TABLE games (
+   gid INT(11) NOT NULL AUTO_INCREMENT,
+   title VARCHAR(60) NOT NULL,
+   starting_money INT(11) NOT NULL,
+   private BOOLEAN NOT NULL DEFAULT FALSE,
+   PRIMARY KEY (gid));
+   
+CREATE TABLE players (
+   pid INT(11) NOT NULL AUTO_INCREMENT,
+   username VARCHAR(60) NOT NULL,
+   gid INT(11) NOT NULL,
+   balance INT(11) NOT NULL,
+   is_admin BOOLEAN NOT NULL DEFAULT FALSE,
+   PRIMARY KEY (pid),
+   CONSTRAINT fk_username_players FOREIGN KEY (username) REFERENCES users (username),
+   CONSTRAINT fk_gid FOREIGN KEY (gid) REFERENCES games (gid));
+   
+CREATE TABLE stocks (
+   sid INT(11) NOT NULL AUTO_INCREMENT,
+   ticker_symbol VARCHAR(10) NOT NULL,
+   pid INT(11) NOT NULL,
+   count INT(11) NOT NULL,
+   PRIMARY KEY (sid),
+   CONSTRAINT fk_pid FOREIGN KEY (pid) REFERENCES players (pid));
+   
+CREATE TABLE transactions (
+   tid INT(11) NOT NULL AUTO_INCREMENT,
+   sid INT(11) NOT NULL,
+   count INT(11) NOT NULL,
+   price INT(11) NOT NULL,       /* In pennies */
+   buy BOOLEAN NOT NULL DEFAULT FALSE,
+   PRIMARY KEY (tid),
+   CONSTRAINT fk_sid FOREIGN KEY (sid) REFERENCES stocks (sid));
