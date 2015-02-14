0758d964e6d55679690f2c414a60ebd61b5fbf21
Dylan Jones <jonesdyn@gmail.com>
2015-02-14 10:37:29 -0600

    Adding startup and shutdown scripts for my development on arch linux

---
+++ script/shutdown.sh
@@ -0,0 +1,3 @@
+sudo systemctl stop tomcat7
+systemctl --user stop eclimd
+sudo systemctl stop mysqld
---
+++ script/startup.sh
@@ -0,0 +1,3 @@
+sudo systemctl start mysqld
+systemctl --user start eclimd
+sudo systemctl start tomcat7
