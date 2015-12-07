3b1bdf7423a752b94d804d16d73dcd6af0116c5b
Dylan Jones <jonesdyn@gmail.com>
2015-12-06 18:54:11 -0600

    Using CDN instead of local javascript and css, fixing tag helpers stuff

--- src/StockSim/Startup.cs
+++ src/StockSim/Startup.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.PlatformAbstractions;
 using StockSim.Data.Access;
 using StockSim.Data.Transfer;
 
@@ -9,6 +12,19 @@ namespace StockSim
 {
    public class Startup
    {
+      public IConfigurationRoot Configuration { get; private set; }
+
+      public Startup(IHostingEnvironment env, IApplicationEnvironment appEnv)
+      {
+         var builder = new ConfigurationBuilder()
+            .SetBasePath(appEnv.ApplicationBasePath);
+            /*.AddJsonFile("appsettings.json")
+            .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true);*/
+
+         builder.AddEnvironmentVariables();
+         Configuration = builder.Build();
+      }
+
       public void ConfigureServices(IServiceCollection services)
       {
          services.AddEntityFramework()
@@ -25,15 +41,13 @@ namespace StockSim
       {
          loggerFactory.MinimumLevel = LogLevel.Information;
          loggerFactory.AddConsole();
+         loggerFactory.AddDebug();
+
          app.UseDeveloperExceptionPage();
-
-         app.UseMvcWithDefaultRoute();
-
-         app.UseWelcomePage();
-
+         //app.UseExceptionHandler("/Home/Error");
          app.UseStaticFiles();
-
          app.UseIdentity();
+         app.UseMvcWithDefaultRoute();
       }
    }
 }
