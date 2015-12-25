07a5764311c1b10380f37dc9d05065901e0ab050
Dylan Jones <jonesdyn@gmail.com>
2015-12-25 04:13:15 +0000

    Note about database migrations

--- src/StockSim/run.sh
+++ src/StockSim/run.sh
@@ -1,1 +1,3 @@
 sudo docker run -t -d -v /sqlite/databases/stocksim.db:/app/stocksim.db -p 80:5004 stocksim
+echo Note if you need to update the databse you must go into the container and run dnx ef database update
+echo Attach to the container using sudo docker exec -i -t cntrid bash
